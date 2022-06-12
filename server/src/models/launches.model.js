1804046aa8fa772872ef19aaf46838da7e63f395
Dorian Thompson <thompsondorian2@gmail.com>
2022-06-11 20:11:46 -0400

    add post and delete request for launch data onto the server

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['NASA', 'ZTM'],
     upcoming: true,
     success: true
@@ -13,10 +15,38 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId){
+    return launches.has(launchId);
+}
+
 function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, { 
+            success: true,
+            upcoming: true,
+            customers: ['Zero To Mastery', 'NASA'],
+            flightNumber: latestFlightNumber
+        })
+    )
+};
+
+function abortLaunchById(launchId){
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+
+    return aborted;
+}
+
 module.exports = {
-    getAllLaunches
+    existsLaunchWithId,
+    getAllLaunches,
+    addNewLaunch,
+    abortLaunchById
 };
